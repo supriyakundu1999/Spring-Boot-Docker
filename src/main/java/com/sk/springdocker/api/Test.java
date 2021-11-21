b70517767828601ca766b6fcf60b3f422c1b235d
Supriya Kundu <supriya.kundu.1999@gmail.com>
2021-11-21 10:46:02 +0530

    Logger Added

--- src/main/java/com/sk/springdocker/api/Test.java
+++ src/main/java/com/sk/springdocker/api/Test.java
@@ -1,5 +1,7 @@
 package com.sk.springdocker.api;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/test")
 public class Test {
 
+	public static final Log LOGGER = LogFactory.getLog(Test.class);
+
 	@GetMapping
 	ResponseEntity<String> getData() {
+		LOGGER.info("GET Method:  Test API");
 		return new ResponseEntity<>("Test API Called..", HttpStatus.OK);
 	}
 
